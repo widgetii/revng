609b084c154777ff81712dd4c10eb816f0a42853
Alessandro Di Federico <ale@rev.ng>
2025-12-03 16:41:59 +0100

    Drop Backend and HeadersGeneration

--- lib/Pipebox/Pipebox.cpp
+++ lib/Pipebox/Pipebox.cpp
@@ -8,8 +8,6 @@
 #include "revng/FunctionIsolation/InvokeIsolatedFunctions.h"
 #include "revng/FunctionIsolation/IsolateFunctions.h"
 #include "revng/FunctionIsolation/PromoteCSVs.h"
-#include "revng/HeadersGeneration/ModelToHeaderPipe.h"
-#include "revng/HeadersGeneration/ModelTypeDefinitionPipe.h"
 #include "revng/Lift/Lift.h"
 #include "revng/Lift/LinkSupportPipe.h"
 #include "revng/Pipebox/LLVMPipe.h"
@@ -31,9 +29,7 @@ using namespace revng::pypeline;
 
 static RegisterContainer<LLVMRootContainer> C1;
 static RegisterContainer<LLVMFunctionContainer> C3;
-static RegisterContainer<CBytesContainer> C4;
 static RegisterContainer<BinariesContainer> C5;
-static RegisterContainer<PTMLCTypeContainer> C6;
 static RegisterContainer<CFGMap> C7;
 static RegisterContainer<HexDumpContainer> C8;
 static RegisterContainer<AssemblyInternalContainer> C9;
@@ -52,8 +48,6 @@ namespace piperuns = revng::pypeline::piperuns;
 static RegisterSingleOutputPipe<Lift> P1;
 static RegisterPipe<PureLLVMPassesRootPipe> P2;
 static RegisterPipe<PureLLVMPassesPipe> P3;
-static RegisterSingleOutputPipe<ModelToHeader> P4;
-static RegisterTypeDefinitionPipe<GenerateModelTypeDefinition> P5;
 static RegisterFunctionPipe<CollectCFG> P6;
 static RegisterFunctionPipe<Isolate> P7;
 static RegisterFunctionPipe<AttachDebugInfo> P8;
